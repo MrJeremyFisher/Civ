d97f4027f38caca6e1d95a85adce64d8a2507e0d
MrJeremyFisher <jeremy@generaleclectic.org>
2026-09-10 13:48:48 -0400

    Per server playercounts

---
+++ plugins/playercounts-velocity/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.shadow)
+}
+
+version = "1.0.0"
+
+dependencies {
+    compileOnly(libs.velocity.api)
+    annotationProcessor(libs.velocity.api)
+
+    api(libs.configurate.yaml)
+}
